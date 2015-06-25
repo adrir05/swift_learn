9a313cf0d4fc0a19d9463e350d854c3a142645d6
adrir05 <adrianrojas@Adrians-MacBook-Pro.local>
2015-06-25 14:36:23 -0600

    onLine [SFMenLife Swift Lesson Video 2]

---
+++ course_project/Playground/PlaygroundLesson1.playground/Contents.swift
@@ -0,0 +1,39 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var texto = "Hello, playground"
+
+texto = "Holis"
+
+var int1 = 3;
+var int2 = 1;
+
+var sum = int1+int2;
+
+//colores
+
+var miColorAzul = UIColor.blueColor();
+miColorAzul
+
+// Vistas
+
+var miVIsta  = UIView (frame: CGRect(x: 0, y: 0, width: 500, height: 750))
+miVIsta.backgroundColor = miColorAzul
+
+//matrices
+
+
+var miMatriz = [[1,2,3],[7,8,9]]
+
+miMatriz
+
+// Clases
+
+class miClase {
+
+    var miPropiedad = "Esta es mi propiedad"
+
+
+}
+
